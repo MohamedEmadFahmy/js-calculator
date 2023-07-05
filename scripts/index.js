7afe34c0e63a089de1b0be2f4799cefa14344274
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-07-06 00:24:51 +0200

    add operation methods

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+
+
+function add(a, b){
+    let num = (a + b);
+    return +num.toFixed(13);
+}
+
+function subtract(a,b){
+    let num = (a - b);
+    return +num.toFixed(13);
+}
+
+function multiply(a,b){
+    let num = (a * b);
+    return +num.toFixed(13);
+}
+
+
+function divide(a,b){
+    let num = (a / b);
+    return +num.toFixed(13);
+}
+
+
+
+
+console.log(add(1,2));
+console.log(multiply(1,2));
+console.log(subtract(1,2));
+console.log(divide(4,3));
+
+
+
+
+
+
+
+
+
