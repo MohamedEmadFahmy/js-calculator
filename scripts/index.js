7bc405b3748cf2da79284a61c5d34873c3d118c7
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2023-07-06 00:33:30 +0200

    add remainder method and operate method

--- scripts/index.js
+++ scripts/index.js
@@ -5,29 +5,59 @@ function add(a, b){
     return +num.toFixed(13);
 }
 
-function subtract(a,b){
+function subtract(a, b){
     let num = (a - b);
     return +num.toFixed(13);
 }
 
-function multiply(a,b){
+function multiply(a, b){
     let num = (a * b);
     return +num.toFixed(13);
 }
 
 
-function divide(a,b){
+function divide(a, b){
     let num = (a / b);
     return +num.toFixed(13);
 }
 
+function remainder(a, b){
+    let num = (a % b);
+    return +num.toFixed(13);
+}
+
+
+let firstNum = 0;
+
+let operation = "";
+
+let secondNum = 0;
 
 
 
-console.log(add(1,2));
-console.log(multiply(1,2));
-console.log(subtract(1,2));
-console.log(divide(4,3));
+function operate(operator, firstNum, secondNum){
+    switch(operator){
+        case "+":
+            return add(firstNum, secondNum);
+        case "-":
+            return subtract(firstNum, secondNum);
+        case "*":
+            return multiply(firstNum, secondNum);
+        case "/":
+            return divide(firstNum, secondNum);
+        case "%":
+            return remainder(firstNum, secondNum);
+        default:
+            return "Math Error";
+    };
+}
+
+console.log(operate("+", 3, 5));
+console.log(operate("%", 5, 4));
+console.log(operate("*", 3, 5));
+console.log(operate("/", 3, 5));
+console.log(operate("/", 5, 4));
+console.log(operate("%", 4, 2));
 
 
 
